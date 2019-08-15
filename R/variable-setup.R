5c25f2b0702427241c891f837c0c6eba6520c5ed
Chris Grandin <cgrandin@shaw.ca>
2019-08-15 14:13:18 -0700

    Add PRD model section

--- R/variable-setup.R
+++ R/variable-setup.R
@@ -133,29 +133,57 @@ end_yr <- 2016
 last_data_yr <- 2016
 this_season <- paste(assess_yr - 1, assess_yr, sep = "/")
 
+
+# -----------------------------------------------------------------------------
+get_vars <- function(region){
+  model_ind <- match(en2fr(region, french), major_regions_short)
+  sbt <- major_models[[model_ind]]$mcmccalcs$sbt.quants
+  sbt_yrs <- as.numeric(colnames(sbt))
+  ## Final year spawning biomass - vector length 4 - 1 = lower, 2 = median, 3 = upper, 4 = mpd
+  final_yr_sbt <- sbt[, sbt_yrs == assess_yr] * 1000
+  refs <- major_models[[model_ind]]$mcmccalcs$r.quants
+  sbo <- refs[rownames(refs) == "sbo",][2:4] * 1000
+  ## Probability that final year biomass is less than 0.3B0 - vector length 3 - 1 = lower, 2 = median, 3 = upper
+  prob_less_03sbo <- refs[rownames(refs) == paste0("psb", assess_yr, "/0.3sbo"),][2:4]
+
+  list(final_yr_sbt = final_yr_sbt,
+       sbo = sbo,
+       prob_less_03sbo = prob_less_03sbo)
+}
 # -----------------------------------------------------------------------------
 # Haida Gwaii-dependent values
-model_ind <- match(en2fr("HG", french), major_regions_short)
-sbt <- major_models[[model_ind]]$mcmccalcs$sbt.quants
-sbt_yrs <- as.numeric(colnames(sbt))
-## Final year spawning biomass - vector length 4 - 1 = lower, 2 = median, 3 = upper, 4 = mpd
-hg_final_yr_sbt <- sbt[,sbt_yrs == assess_yr] * 1000
-hg_refs <- major_models[[model_ind]]$mcmccalcs$r.quants
-hg_sbo <- hg_refs[rownames(hg_refs) == "sbo",][2:4] * 1000
-## Probability that final year biomass is less than 0.3B0 - vector length 3 - 1 = lower, 2 = median, 3 = upper
-hg_prob_less_03sbo <- hg_refs[rownames(hg_refs) == paste0("psb", assess_yr, "/0.3sbo"),][2:4]
+hg_vars <- get_vars("HG")
+hg_final_yr_sbt <- hg_vars[[1]]
+hg_sbo <- hg_vars[[2]]
+hg_prob_less_03sbo <- hg_vars[[3]]
 
 # -----------------------------------------------------------------------------
 # Prince Rupert District-dependent values
+prd_vars <- get_vars("PRD")
+prd_final_yr_sbt <- prd_vars[[1]]
+prd_sbo <- prd_vars[[2]]
+prd_prob_less_03sbo <- prd_vars[[3]]
 
 # -----------------------------------------------------------------------------
 # Central Coast-dependent values
+cc_vars <- get_vars("CC")
+cc_final_yr_sbt <- cc_vars[[1]]
+cc_sbo <- cc_vars[[2]]
+cc_prob_less_03sbo <- cc_vars[[3]]
 
 # -----------------------------------------------------------------------------
 # Strait of Georgia District-dependent values
+sog_vars <- get_vars("SoG")
+sog_final_yr_sbt <- sog_vars[[1]]
+sog_sbo <- sog_vars[[2]]
+sog_prob_less_03sbo <- sog_vars[[3]]
 
 # -----------------------------------------------------------------------------
 # West Coast Vancouver Island-dependent values
+wcvi_vars <- get_vars("WCVI")
+wcvi_final_yr_sbt <- wcvi_vars[[1]]
+wcvi_sbo <- wcvi_vars[[2]]
+wcvi_prob_less_03sbo <- wcvi_vars[[3]]
 
 # Age to highlight in figure
 ageShow <- 3
