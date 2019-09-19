6a3920201b4cc3b20c2765d5ba521e624a67df29
Chris Grandin <cgrandin@shaw.ca>
2019-09-19 08:36:35 -0700

    Add a script which pulls, rebases, and installs all packages required for herringsr

---
+++ R/update_packages.R
@@ -0,0 +1,13 @@
+git_dir <- "C:/github"
+curr_dir <- getwd()
+shell(paste0("cd ", file.path(git_dir, "gfutilities"), " && git pull --rebase"))
+shell(paste0("cd ", file.path(git_dir, "gfiscamutils"), " && git pull --rebase"))
+shell(paste0("cd ", file.path(git_dir, "rosettafish"), " && git pull --rebase"))
+shell(paste0("cd ", file.path(git_dir, "herringutils"), " && git pull --rebase"))
+
+setwd(git_dir)
+devtools::install("rosettafish", quick = TRUE, dependencies = FALSE)
+devtools::install("gfutilities", quick = TRUE, dependencies = FALSE)
+devtools::install("gfiscamutils", quick = TRUE, dependencies = FALSE)
+devtools::install("herringutils", quick = TRUE, dependencies = FALSE)
+setwd(curr_dir)
