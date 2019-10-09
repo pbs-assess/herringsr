8c40927c898eb953601dda5d0e4a4d32654cddb7
Chris Grandin <cgrandin@shaw.ca>
2019-10-09 10:18:25 -0700

    Add batch script to run the mcevals again for all models

---
+++ run-mcevals.bat
@@ -0,0 +1,15 @@
+cd models/CC/AM2
+iscam -mceval
+mv *.csv mcmc
+cd ../../HG/AM2
+iscam -mceval
+mv *.csv mcmc
+cd ../../PRD/AM2
+iscam -mceval
+mv *.csv mcmc
+cd ../../SoG/AM2
+iscam -mceval
+mv *.csv mcmc
+cd ../../WCVI/AM2
+iscam -mceval
+mv *.csv mcmc
