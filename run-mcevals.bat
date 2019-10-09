f0458e756fc0b96b47fc5e141b0049e2be2b1fba
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-10-09 13:30:42 -0700

    Remove mcmc folders if present

--- run-mcevals.bat
+++ run-mcevals.bat
@@ -1,15 +1,25 @@
 cd models/CC/AM2
+if exist mcmc rd /s /q mcmc
+md mcmc
 iscam -mceval
 mv *.csv mcmc
 cd ../../HG/AM2
+if exist mcmc rd /s /q mcmc
+md mcmc
 iscam -mceval
 mv *.csv mcmc
 cd ../../PRD/AM2
+if exist mcmc rd /s /q mcmc
+md mcmc
 iscam -mceval
 mv *.csv mcmc
 cd ../../SoG/AM2
+if exist mcmc rd /s /q mcmc
+md mcmc
 iscam -mceval
 mv *.csv mcmc
 cd ../../WCVI/AM2
+if exist mcmc rd /s /q mcmc
+md mcmc
 iscam -mceval
 mv *.csv mcmc
