f6499502402bf59be26ade24399806d1729199ef
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-10-24 10:24:37 -0700

    Ignore rebase for grinnell -- doesn't work

--- R/update_packages.R
+++ R/update_packages.R
@@ -3,18 +3,20 @@ if( tolower(Sys.getenv("USERNAME")) == "grinnellm" )  git_dir <- "C:/Grinnell/Gi
 if( tolower(Sys.getenv("USERNAME")) == "grandin" )  git_dir <- "C:/github/pbs-assess"
 curr_dir <- getwd()
 
-cat(crayon::green("\nRebasing new commits from rosettafish...\n"))
-shell(paste0("cd ", file.path(git_dir, "rosettafish"), " && git pull --rebase"))
-cat(crayon::green("\nRebasing new commits from gfutilities...\n"))
-shell(paste0("cd ", file.path(git_dir, "gfutilities"), " && git pull --rebase"))
-cat(crayon::green("\nRebasing new commits from gfiscamutils...\n"))
-shell(paste0("cd ", file.path(git_dir, "gfiscamutils"), " && git pull --rebase"))
-cat(crayon::green("\nRebasing new commits from herringutils...\n"))
-shell(paste0("cd ", file.path(git_dir, "herringutils"), " && git pull --rebase"))
-cat(crayon::green("\nRebasing new commits from csasdown...\n"))
-shell(paste0("cd ", file.path(git_dir, "csasdown"), " && git pull --rebase"))
-cat(crayon::green("\nRebasing new commits from herringsr...\n"))
-shell(paste0("cd ", file.path(git_dir, "herringsr"), " && git pull --rebase"))
+if( tolower(Sys.getenv("USERNAME")) != "grinnellm" ) {
+  cat(crayon::green("\nRebasing new commits from rosettafish...\n"))
+  shell(paste0("cd ", file.path(git_dir, "rosettafish"), " && git pull --rebase"))
+  cat(crayon::green("\nRebasing new commits from gfutilities...\n"))
+  shell(paste0("cd ", file.path(git_dir, "gfutilities"), " && git pull --rebase"))
+  cat(crayon::green("\nRebasing new commits from gfiscamutils...\n"))
+  shell(paste0("cd ", file.path(git_dir, "gfiscamutils"), " && git pull --rebase"))
+  cat(crayon::green("\nRebasing new commits from herringutils...\n"))
+  shell(paste0("cd ", file.path(git_dir, "herringutils"), " && git pull --rebase"))
+  cat(crayon::green("\nRebasing new commits from csasdown...\n"))
+  shell(paste0("cd ", file.path(git_dir, "csasdown"), " && git pull --rebase"))
+  cat(crayon::green("\nRebasing new commits from herringsr...\n"))
+  shell(paste0("cd ", file.path(git_dir, "herringsr"), " && git pull --rebase"))
+}
 
 setwd(git_dir)
 cat(crayon::green("\nBuilding and installing rosettafish package...\n"))
