f38ef088fa1f99f2ce099dafa0ce55e6ff6ca67b
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-11-04 09:51:16 -0800

    Clean up more temporary files

--- fresh.bat
+++ fresh.bat
@@ -1,11 +1,13 @@
-# Clean up the directory after a building the document (Windows version)
+# Clean up the directory after building the document (Windows version)
 
+del sr.aux
+del sr.knit.md
 del sr.log
+del sr.pdf
 del sr.Rmd
 del sr.tex
 del sr.upa
-del sr.aux
-del sr.pdf
+del sr.utf8.md
 
 rmdir /S /Q csas-style
 rmdir /S /Q knitr-figs-pdf
