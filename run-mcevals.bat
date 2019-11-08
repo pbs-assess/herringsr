c80976391077b9e2ff30e011fa369ea942cec128
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2019-11-08 12:02:34 -0800

    Ask before running mcevals
    Prevent running accidentally, i.e., when trying to run fresh.bat

--- run-mcevals.bat
+++ run-mcevals.bat
@@ -1,25 +1,41 @@
+@echo off
+
+setlocal
+:PROMPT
+SET /P CONTINUE=Do you want to run mcmcevals (Y/N)?
+IF /I "%CONTINUE%" NEQ "Y" GOTO END
+
+@echo on
+
 cd models/HG/AM2
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
+
 cd ../../PRD/AM2
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
+
 cd ../../CC/AM2
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
+
 cd ../../SoG/AM2
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
+
 cd ../../WCVI/AM2
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
+
+:END
+endlocal
