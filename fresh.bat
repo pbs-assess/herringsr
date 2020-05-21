f14042600a5474cf5c4877b5cbb562fcd51778fa
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-05-21 12:06:04 -0700

    Change name from sr to HerringSR

--- fresh.bat
+++ fresh.bat
@@ -1,13 +1,13 @@
 # Clean up the directory after building the document (Windows version)
 
-del sr.aux
-del sr.knit.md
-del sr.log
-del sr.pdf
-del sr.Rmd
-del sr.tex
-del sr.upa
-del sr.utf8.md
+del HerringSR.aux
+del HerringSR.knit.md
+del HerringSR.log
+del HerringSR.pdf
+del HerringSR.Rmd
+del HerringSR.tex
+del HerringSR.upa
+del HerringSR.utf8.md
 
 rmdir /S /Q csas-style
 rmdir /S /Q knitr-figs-pdf
