a41635420a12e60cb7dbc45ca4aa82e0a3fbce52
Shawkshaw <scballar@gmail.com>
2020-07-24 13:58:26 -0700

    Updating assess_yr to 2020

--- R/variable-setup.R
+++ R/variable-setup.R
@@ -1,4 +1,4 @@
-assess_yr <- 2019 #as.numeric(substr(Sys.Date(), 1, 4))
+assess_yr <- 2020 #as.numeric(substr(Sys.Date(), 1, 4))
 last_assess_yr <- assess_yr - 1
 this_season <- paste(assess_yr - 1, assess_yr, sep = "/")
 
