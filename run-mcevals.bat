3dd656ef7d098b594bd960260823a71d97da2441
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2022-07-19 14:31:00 -0700

    Add iscam and mcmc to run.bat

--- run-mcevals.bat
+++ run-mcevals.bat
@@ -8,30 +8,40 @@ IF /I "%CONTINUE%" NEQ "Y" GOTO END
 @echo on
 
 cd models/HG/AM2
+iscam
+iscam -mcmc 10000 -mcsave 10
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
 
 cd ../../PRD/AM2
+iscam
+iscam -mcmc 10000 -mcsave 10
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
 
 cd ../../CC/AM2
+iscam
+iscam -mcmc 10000 -mcsave 10
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
 
 cd ../../SoG/AM2
+iscam
+iscam -mcmc 10000 -mcsave 10
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
 mv *.csv mcmc
 
 cd ../../WCVI/AM2
+iscam
+iscam -mcmc 10000 -mcsave 10
 if exist mcmc rd /s /q mcmc
 md mcmc
 iscam -mceval
