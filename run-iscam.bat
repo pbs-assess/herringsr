9886d3e2fb46223727abf4d89698aaa4ba0c0081
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2022-07-21 14:09:50 -0700

    Update iscam bat to clean up folder

--- run-iscam.bat
+++ run-iscam.bat
@@ -5,46 +5,104 @@ setlocal
 SET /P CONTINUE=Do you want to run the SCA model (Y/N)?
 IF /I "%CONTINUE%" NEQ "Y" GOTO END
 
+set /A num_mcmc = 20000
+set /A nth_save = 10
+
 @echo on
 
 cd models/HG
-iscam
-iscam -mcmc 20000 -mcsave 10
 if exist mcmc rd /s /q mcmc
-md mcmc
+if exist retrospectives rd /s /q retrospectives
+if exist temp rd /s /q temp
+md temp
+mv *.ctl temp
+mv *.dat temp
+mv *.pfc temp
+del /q *.*
+mv temp/*.ctl .
+mv temp/*.dat .
+mv temp/*.pfc .
+rd /s /q temp
+iscam
+iscam -mcmc %num_mcmc% -mcsave %nth_save%
 iscam -mceval
+md mcmc
 mv *.csv mcmc
 
 cd ../PRD
-iscam
-iscam -mcmc 20000 -mcsave 10
 if exist mcmc rd /s /q mcmc
-md mcmc
+if exist retrospectives rd /s /q retrospectives
+if exist temp rd /s /q temp
+md temp
+mv *.ctl temp
+mv *.dat temp
+mv *.pfc temp
+del /q *.*
+mv temp/*.ctl .
+mv temp/*.dat .
+mv temp/*.pfc .
+rd /s /q temp
+iscam
+iscam -mcmc %num_mcmc% -mcsave %nth_save%
 iscam -mceval
+md mcmc
 mv *.csv mcmc
 
 cd ../CC
-iscam
-iscam -mcmc 20000 -mcsave 10
 if exist mcmc rd /s /q mcmc
-md mcmc
+if exist retrospectives rd /s /q retrospectives
+if exist temp rd /s /q temp
+md temp
+mv *.ctl temp
+mv *.dat temp
+mv *.pfc temp
+del /q *.*
+mv temp/*.ctl .
+mv temp/*.dat .
+mv temp/*.pfc .
+rd /s /q temp
+iscam
+iscam -mcmc %num_mcmc% -mcsave %nth_save%
 iscam -mceval
+md mcmc
 mv *.csv mcmc
 
 cd ../SoG
-iscam
-iscam -mcmc 20000 -mcsave 10
 if exist mcmc rd /s /q mcmc
-md mcmc
+if exist retrospectives rd /s /q retrospectives
+if exist temp rd /s /q temp
+md temp
+mv *.ctl temp
+mv *.dat temp
+mv *.pfc temp
+del /q *.*
+mv temp/*.ctl .
+mv temp/*.dat .
+mv temp/*.pfc .
+rd /s /q temp
+iscam
+iscam -mcmc %num_mcmc% -mcsave %nth_save%
 iscam -mceval
+md mcmc
 mv *.csv mcmc
 
 cd ../WCVI
-iscam
-iscam -mcmc 20000 -mcsave 10
 if exist mcmc rd /s /q mcmc
-md mcmc
+if exist retrospectives rd /s /q retrospectives
+if exist temp rd /s /q temp
+md temp
+mv *.ctl temp
+mv *.dat temp
+mv *.pfc temp
+del /q *.*
+mv temp/*.ctl .
+mv temp/*.dat .
+mv temp/*.pfc .
+rd /s /q temp
+iscam
+iscam -mcmc %num_mcmc% -mcsave %nth_save%
 iscam -mceval
+md mcmc
 mv *.csv mcmc
 
 :END
