1272712c9aea2cb214440ce0a6c13b485a8335aa
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2022-07-27 14:46:23 -0700

    Batch loop over SARs

--- run-iscam.bat
+++ run-iscam.bat
@@ -7,113 +7,34 @@ IF /I "%CONTINUE%" NEQ "Y" GOTO END
 
 set /A num_mcmc = 20000
 set /A nth_save = 10
+set "list=HG PRD CC SoG WCVI"
 
-@echo on
+cd models
 
-cd models/HG
-if exist mcmc rd /s /q mcmc
-if exist retrospectives rd /s /q retrospectives
-if exist temp rd /s /q temp
-md temp
-mv *.ctl temp
-mv *.dat temp
-mv *.pfc temp
-mv iscam.exe temp
-del /q *.*
-mv temp/*.ctl .
-mv temp/*.dat .
-mv temp/*.pfc .
-mv temp/iscam.exe .
-rd /s /q temp
-iscam
-iscam -mcmc %num_mcmc% -mcsave %nth_save%
-iscam -mceval
-md mcmc
-mv *.csv mcmc
-
-cd ../PRD
-if exist mcmc rd /s /q mcmc
-if exist retrospectives rd /s /q retrospectives
-if exist temp rd /s /q temp
-md temp
-mv *.ctl temp
-mv *.dat temp
-mv *.pfc temp
-mv iscam.exe temp
-del /q *.*
-mv temp/*.ctl .
-mv temp/*.dat .
-mv temp/*.pfc .
-mv temp/iscam.exe .
-rd /s /q temp
-iscam
-iscam -mcmc %num_mcmc% -mcsave %nth_save%
-iscam -mceval
-md mcmc
-mv *.csv mcmc
-
-cd ../CC
-if exist mcmc rd /s /q mcmc
-if exist retrospectives rd /s /q retrospectives
-if exist temp rd /s /q temp
-md temp
-mv *.ctl temp
-mv *.dat temp
-mv *.pfc temp
-mv iscam.exe temp
-del /q *.*
-mv temp/*.ctl .
-mv temp/*.dat .
-mv temp/*.pfc .
-mv temp/iscam.exe .
-rd /s /q temp
-iscam
-iscam -mcmc %num_mcmc% -mcsave %nth_save%
-iscam -mceval
-md mcmc
-mv *.csv mcmc
-
-cd ../SoG
-if exist mcmc rd /s /q mcmc
-if exist retrospectives rd /s /q retrospectives
-if exist temp rd /s /q temp
-md temp
-mv *.ctl temp
-mv *.dat temp
-mv *.pfc temp
-mv iscam.exe temp
-del /q *.*
-mv temp/*.ctl .
-mv temp/*.dat .
-mv temp/*.pfc .
-mv temp/iscam.exe .
-rd /s /q temp
-iscam
-iscam -mcmc %num_mcmc% -mcsave %nth_save%
-iscam -mceval
-md mcmc
-mv *.csv mcmc
-
-cd ../WCVI
-if exist mcmc rd /s /q mcmc
-if exist retrospectives rd /s /q retrospectives
-if exist temp rd /s /q temp
-md temp
-mv *.ctl temp
-mv *.dat temp
-mv *.pfc temp
-mv iscam.exe temp
-del /q *.*
-mv temp/*.ctl .
-mv temp/*.dat .
-mv temp/*.pfc .
-mv temp/iscam.exe .
-rd /s /q temp
-iscam
-iscam -mcmc %num_mcmc% -mcsave %nth_save%
-iscam -mceval
-md mcmc
-mv *.csv mcmc
+for /D %%d in (%list%) do (
+  cd %%d
+  echo Running SCA model in %cd%\%%d
+  if exist mcmc rd /s /q mcmc
+  if exist retrospectives rd /s /q retrospectives
+  if exist temp rd /s /q temp
+  md temp
+  mv *.ctl temp
+  mv *.dat temp
+  mv *.pfc temp
+  mv iscam.exe temp
+  del /q *.*
+  mv temp/*.ctl .
+  mv temp/*.dat .
+  mv temp/*.pfc .
+  mv temp/iscam.exe .
+  rd /s /q temp
+  iscam
+  iscam -mcmc %num_mcmc% -mcsave %nth_save%
+  iscam -mceval
+  md mcmc
+  mv *.csv mcmc
+  cd ..
+)
 
 :END
 endlocal
