85096cac05957bf22f6d6fcb4cc4bb7fa7d5b544
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2023-08-16 10:05:56 -0700

    Update prod_period format

--- R/variable-setup.R
+++ R/variable-setup.R
@@ -236,11 +236,34 @@ wcvi_vars <- get_vars("WCVI", french = french)
 
 # Years and proportion for productive period
 prod_period <- list(
-  HG = list(region = "Haida Gwaii", yrs = 1975:1985, prop = 0.75),
-  PRD = list(region = "Prince Rupert District", yrs = 1983:1992, prop = 1.0),
-  CC = list(region = "Central Coast", yrs = 1990:1999, prop = 1.0),
-  SoG = list(region = "Strait of Georgia", yrs = 1988:2007, prop = 0.8),
+  HG = list(
+    region = "HG",
+    region_name = "Haida Gwaii",
+    yrs = 1975:1985,
+    prop = 0.75
+  ),
+  PRD = list(
+    region = "PRD",
+    region_name = "Prince Rupert District",
+    yrs = 1983:1992,
+    prop = 1.0
+  ),
+  CC = list(
+    region = "CC",
+    region_name = "Central Coast",
+    yrs = 1990:1999,
+    prop = 1.0
+  ),
+  SoG = list(
+    region = "SoG",
+    region_name = "Strait of Georgia",
+    yrs = 1988:2007,
+    prop = 0.8
+  ),
   WCVI = list(
-    region = "West Coast of Vancouver Island", yrs = 1990:1999, prop = 1.0
+    region = "WCVI",
+    region_name = "West Coast of Vancouver Island",
+    yrs = 1990:1999,
+    prop = 1.0
   )
 )
